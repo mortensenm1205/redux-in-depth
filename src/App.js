d16770088b0a62c8084c167d6dfd70a2165e07b1
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-05 17:43:51 -0600

    refactor App.js to be container

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react';
+import Input from './components/Input';
+import Button from './components/Button';
 import * as actionTypes from './actions/types';
-// yes, store is setup in root index. But we need it hear because i can
-// attach the dispatch call to the UI button, which will call to the store to
-// have the reducer update state and return it back to where its being called
-// with store.getState() to which i have transfered that to this.props.values
-import { store } from './index';
 
 class App extends Component {
 
   state = {
-    localText: ""
+    displayText: ""
+  }
+
+  handleChange = e => {
+      this.setState({ displayText: e.target.value })
+  }
+
+  handleClick = () => {
+      const { displayText } = this.state;
+      const { store } = this.props;
+      this.setState({ displayText: "" })
+      store.dispatch({ type: actionTypes.UPDATING_TEXT, text: displayText })
   }
 
   render() {
-    const { localText } = this.state;
-    // This is our store state coming in via props
+    const { displayText } = this.state;
     const { values } = this.props;
     return (
       <div>
-        <input type="text" value={localText} onChange={e => {
-            this.setState({ localText: e.target.value })
-        }}/>
-        <button onClick={() => {
-            this.setState({ localText: "" })
-            store.dispatch({ type: actionTypes.UPDATING_TEXT, text: localText })
-        }}>
-        Add Text
-        </button>
+        <Input displayText={displayText} change={this.handleChange}/>
+        <Button clicked={this.handleClick}/>
         <span>{values}</span>
       </div>
     );
