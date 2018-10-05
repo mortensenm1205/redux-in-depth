99558ab137d681df163e1b832904062f8d921633
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-05 17:09:58 -0600

    UI is now reacting to store dispatch calls and displaying updated state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import * as actions from './actions/types';
+import * as actionTypes from './actions/types';
+// yes, store is setup in root index. But we need it hear because i can
+// attach the dispatch call to the UI button, which will call to the store to
+// have the reducer update state and return it back to where its being called
+// with store.getState() to which i have transfered that to this.props.values
+import { store } from './index';
 
 class App extends Component {
+
+  state = {
+    localText: ""
+  }
+
   render() {
-    const { storeToTest } = this.props;
+    const { localText } = this.state;
+    // This is our store state coming in via props
+    const { values } = this.props;
     return (
       <div>
+        <input type="text" value={localText} onChange={e => {
+            this.setState({ localText: e.target.value })
+        }}/>
         <button onClick={() => {
-            storeToTest.dispatch({ type: actions.UPDATING_TEXT, text: "Hello" })
+            this.setState({ localText: "" })
+            store.dispatch({ type: actionTypes.UPDATING_TEXT, text: localText })
         }}>
         Add Text
         </button>
+        <span>{values}</span>
       </div>
     );
   }
