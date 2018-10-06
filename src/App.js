133d9d4fe0ec4d0f0fc3097365fef6d2ef7aba63
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-05 18:26:06 -0600

    App now supports Display comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Input from './components/Input';
 import Button from './components/Button';
+import Display from './components/Display';
 import * as actionTypes from './actions/types';
 
 class App extends Component {
@@ -20,16 +21,19 @@ class App extends Component {
       store.dispatch({ type: actionTypes.UPDATING_TEXT, text: displayText })
   }
 
+  handleRemoval = () => {
+      const { store } = this.props;
+      store.dispatch({ type: actionTypes.REMOVING_TEXT })
+    };
+
   render() {
     const { displayText } = this.state;
-    const { values, store } = this.props;
+    const { values } = this.props;
     return (
       <div>
         <Input displayText={displayText} change={this.handleChange}/>
         <Button clicked={this.handleUpdate}/>
-        <span onClick={() => {
-            store.dispatch({ type: actionTypes.REMOVING_TEXT })
-          }}>{values}</span>
+        <Display values={values} remove={this.handleRemoval} />
       </div>
     );
   }
