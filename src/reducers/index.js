6ee932547d2028fe8d638c6399c7e054534b5271
Matt Mortensen <mortensenm1205@gmail.com>
2018-10-06 12:35:12 -0600

    proper removal state item

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,23 +3,23 @@ import { combineReducers } from 'redux';
 
 const text = (state = [], action) => {
   switch(action.type) {
-    case actionTypes.ADDING_TEXT:
-      return state.map((s, index) => {
-        return {
-          ...s,
-          id: index, 
-          text: action.text 
-        }
-      })
-    // case actionTypes.ADDING_TEXT: 
-    //   return [
-    //     ...state,
-    //     { id: action.id, text: action.text}
-    //   ]
+    // case actionTypes.ADDING_TEXT:
+    //   return state.map((s, index) => {
+    //     return {
+    //       ...s,
+    //       id: index, 
+    //       text: action.text 
+    //     }
+    //   })
+    case actionTypes.ADDING_TEXT: 
+      return [
+        ...state,
+        { id: action.id, text: action.text}
+      ]
     case actionTypes.REMOVING_TEXT:
       return [
         ...state.slice(0, action.id),
-        state.slice(action.id, 0),
+        ...state.slice(action.id, 0),
         ...state.slice(action.id + 1)
       ];
     default:
